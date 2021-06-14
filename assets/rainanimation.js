f4ace0eb385e60809fba424c67407b479c6777f3
buianhtuan2903 <buianhtuan2903@gmail.com>
2021-06-15 00:32:25 +0700

    update

--- assets/rainanimation.js
+++ assets/rainanimation.js
@@ -6,21 +6,24 @@ var makeItRain = function() {
     var drops = "";
     var backDrops = "";
   
-    while (increment < 85) {
+    while (increment < 93) {
       //couple random numbers to use for various randomizations
       //random number between 98 and 1
       var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
       //random number between 5 and 2
       var randoFiver = (Math.floor(Math.random() * (2 - 2 + 1) + 2));
       //increment
-      increment += (Math.floor(Math.random() * (14 - 1 + 1) + 1));
+      increment += (Math.floor(Math.random() * (28 - 1 + 1) + 1));
+      if(increment > 100){
+        increment = 98; 
+      }
       //add in a new raindrop with various randomizations to certain CSS properties
-      drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver + 120) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.' + randoHundo + 's;"></div></div>';
+      drops += '<div class="drop" style="left: ' + increment + '%; top: ' + (randoFiver - 200) + 'px; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.' + randoHundo + 's;"></div></div>';
       // backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div>';
     }
   
     $('.rain.front-row').append(drops);
-    $('.rain.back-row').append(backDrops);
+    // $('.rain.back-row').append(backDrops);
 
 
   }
